d040eca8e94adbcea36f024889effbc7a85c9760
Sshashank5690 <78215769+Sshashank5690@users.noreply.github.com>
2023-07-31 16:05:06 +0530

    header changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/header";
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -22,7 +23,8 @@ export default function RootLayout({
         <div className="bg-[#dad7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] 
         rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] 
         xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-
+      
+      <Header />
       {children}
       </body>
     </html>
